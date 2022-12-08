0507d189a0c62ca9f4e51429adde72040ccdee7b
yayayang <hana12312347@gmail.com>
2022-12-08 18:38:50 +0800

    1-1

---
+++ 1-1.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	printf("H\tH\t   A\n");
+	printf("H\tH\t  A A\n");
+	printf("HHHHHHHHH\t A   A\n");
+	printf("H\tH\tAAAAAAA\n");
+	printf("H\tH\tA     A\n");
+	return 0;
+ } 
